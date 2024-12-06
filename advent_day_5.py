387aa5ff29d6ab9e4e524016ba03f91e161138af
Rob Chavez <42754801+rob-chavez@users.noreply.github.com>
2024-12-05 19:23:29 -0500

    Update advent_day_5.py

--- advent_day_5.py
+++ advent_day_5.py
@@ -1,27 +1,18 @@
 FILENAME = "/Users/blackbox/Desktop/advent5"
-f = open(FILENAME)
-strings = []
-for line in f:
-    strings.append(line.rstrip())
-f.close()
 
-rules = {}
-printjobs = []
+def load_data(FILENAME):
+    with open(FILENAME) as f:
+        lines = [line.rstrip() for line in f]
 
-
-for line in strings:
-    if '|' in line:
-        r1, r2 = line.split('|')
-        if int(r2) in rules:
-            update = rules[int(r2)]
-            update.append(int(r1))
-        else:
-            rules[int(r2)] = [int(r1)]
-    elif "," in line:
-        printjob = line.split(',')
-        printjobs.append([int(page) for page in printjob])
-    else:
-        pass
+    rules, printjobs = {}, []
+    for line in lines:
+        if '|' in line:
+            r1, r2 = map(int, line.split('|'))
+            rules.setdefault(r2, []).append(r1)
+        elif ',' in line:
+            printjobs.append([int(page) for page in line.split(',')])
+    
+    return rules, printjobs
     
 def mid(input_list):
     middle = float(len(input_list))/2
@@ -32,93 +23,42 @@ def mid(input_list):
     
 
 def valid(printjobs, part1=True):
-    validprintjob = []
-    notvalidprintjob = []
-
+    validprintjob, notvalidprintjob = [], []
     for printjob in printjobs:
-        #print(printjob)
-        for page_index, page_to_be_printed in enumerate(printjob):
-            rule_broken = 0
-            try:
-                rules_for_page_to_be_printed = rules[page_to_be_printed]
-                pages_still_to_be_printed = printjob[page_index:]
-
-                for page_still_to_be_printed in pages_still_to_be_printed:
-                    if page_still_to_be_printed in rules_for_page_to_be_printed:
-                        rule_broken = 1
-                        break
-
-                if rule_broken == 1:
-                    break
-            except:
-                continue
-
-        if rule_broken == 1:
-            notvalidprintjob.append(printjob)
+        for page_index, page in enumerate(printjob):
+            if any(p in rules.get(page, []) for p in printjob[page_index:]):
+                notvalidprintjob.append(printjob)
+                break
         else:
             validprintjob.append(printjob)
-    
-    if part1:
-        return(validprintjob, notvalidprintjob)
-    else:
-        if len(notvalidprintjob) == 0:
-            return (1,0)
-        else:
-            return (0, page_index)
+
+    return (validprintjob, notvalidprintjob) if part1 else ((1, 0) if not notvalidprintjob else (0, page_index))
 
 
 def highest_index(page, job):
-    highest_index = 0
-    for index, pg in enumerate(job):
-        if pg in rules[page]:
-            highest_index = index
-    return highest_index
+    return next((i for i in range(len(job) - 1, -1, -1) if job[i] in rules[page]), 0)
 
 def get_index(page, job):
     return job.index(page)
 
 def iterfix(page, pagelist):
-    
-    hi = highest_index(page, pagelist)
-    page_index = get_index(page, pagelist)
-    pagelist.pop(page_index)
-    pagelist.insert(hi, page)
-    
+    pagelist.insert(highest_index(page, pagelist), pagelist.pop(get_index(page, pagelist)))
     return pagelist
 
-corrected = []
-
 def part1(printjobs):
-    (validprintjob, notvalidprintjob) = valid(printjobs)
-    value = 0
-    for validjob in validprintjob:
-        value+=mid(validjob)
-    print(value)
-    return notvalidprintjob
-
-    
-
+    print(sum(mid(job) for job in valid(printjobs)[0]))
+    return valid(printjobs)[1]
     
 def part2(notvalidprintjob):
-    index=0
+    corrected = []
     for nvpj in notvalidprintjob:
-
         pagelist = nvpj.copy()
-        page = pagelist[index]
-        val,error = valid([pagelist], False)
+        while not (val := valid([pagelist], False))[0]:
+            pagelist = iterfix(pagelist[val[1]], pagelist)
+        corrected.append(pagelist)
 
-        while not val:
-            pagelist = iterfix(page, pagelist)
-            val,error = valid([pagelist], False)
-            page = pagelist[error]
-
-        corrected.append(pagelist.copy())
-
-    value = 0
-    for validjob in corrected:
-        value+=mid(validjob)
-    print(value)
+    print(sum(mid(job) for job in corrected))
 
     
-notvalidprintjob = part1(printjobs)
-part2(notvalidprintjob)
+rules, printjobs = load_data(FILENAME)    
+part2(part1(printjobs))
